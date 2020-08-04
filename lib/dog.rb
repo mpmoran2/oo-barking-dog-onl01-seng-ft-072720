e4384e44df08706df17e6a0486f5e54af03395ce
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-04 16:21:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,18 @@
 class Dog 
   def name=(doge_name)
-    this_doge = doge_name
+    @this_doge = doge_name
   end
   def name
-    this_doge
+    @this_doge
   end
   def bark
     puts "woof!"
   end
 end
+
+frodo =  Dog.new 
+frodo.name = "Froddie"
+
+puts frodo.name
+
+#rip frodo, my best little man
