6bb366053bb4a345bee83affdd9fd95ba57ec89b
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-04 16:21:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog 
+  def name=(doge_name)
+    @this_doge = doge_name
+  end
+  def name
+    @this_doge
+  end
+  def bark
+    puts "woof!"
+  end
+end
+
+frodo =  Dog.new 
+frodo.name = "Froddie"
+
+puts frodo.name
+
+#rip frodo, my best little man
